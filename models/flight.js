7ef95c51e10295311da341cafe848da960b8e882
Justin Vongdara <vongdara16@gmail.com>
2022-02-28 16:27:36 -0500

    build flight model and config

---
+++ models/flight.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+
+const Schema = flight.Schema
+
+const flightSchema = new Schema({
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United']
+  },
+  aiport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+    default: 'DEN'
+  }, 
+  flightNo: {
+    type: Number,
+    min: 10,
+    max: 9999
+  } ,
+  departs: {
+    type: Date(),
+    default: function(){
+      return new (Date().getFullYear()+1)
+    }
+  }
+})
