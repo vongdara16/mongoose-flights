08294f9f69e512e8fa680cec1dff27560c930816
Justin Vongdara <vongdara16@gmail.com>
2022-03-01 19:31:09 -0500

    complete exercise 1 and 2 for lab part 3

--- models/flight.js
+++ models/flight.js
@@ -34,7 +34,10 @@ const flightSchema = new Schema({
     type: Date,
     default: Date.now() + 365*24*60*60*1000
   },
-  tickets: [ticketSchema]
+  tickets: [ticketSchema],
+  meals: [{ type: Schema.Types.ObjectId, ref: 'Meal'}]
+}, {
+  timestamps: true
 })
 
 const Flight = mongoose.model('Flight', flightSchema)
---
+++ models/meal.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+const mealSchema = new Schema({
+  name: String,
+})
+
+const Meal = mongoose.model('Meal', mealSchema)
+
+export {
+  Meal
+}
